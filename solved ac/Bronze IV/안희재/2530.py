0b2bddca488253700c1b47455d93755aca46a720
heejae <marioahn@naver.com>
2022-02-12 00:01:17 +0900

    HJ 0212

--- solved ac/Bronze IV/안희재/2530.py
+++ solved ac/Bronze IV/안희재/2530.py
@@ -1,11 +1,36 @@
 a, b, c = map(int,input().split())
 d = int(input())
 
-if c + d < 60:
-    print(a, b, c+d)
-elif b+(c+d)//60 < 60:
-    print(a, b+(c+d)//60, b+(c+d)%60)
-elif a + b + (c+d)//60 >= 24:
-    print(a+b+(c+d)//60)
+sum = a * 3600 + b * 60 + c
+plus_sum = sum + d
 
-# ?????????????? 오늘은 무리데스,,
+hour = plus_sum // 3600
+minute = plus_sum % 3600 // 60
+second = plus_sum % 3600 % 60
+
+if hour >= 24:
+    hour -=24
+
+print(hour, minute, second)
+
+# 아래 코드 이해하기
+# H, M, S = map(int, input().split())
+# D = int(input()) 
+
+# S += D % 60
+# D = D // 60
+# if S >= 60:
+#     S -= 60
+#     M += 1
+
+# M += D % 60
+# D = D // 60
+# if M >= 60:
+#     M -= 60
+#     H += 1
+
+# H += D % 24 -> 60이 아니라 24임 (시만 유일하게 1~23니까)
+# if H >= 24:
+#     H -= 24
+
+# print(H,M,S)
