d4db40fda59525973c202c0cff87cd8119161682
오서준 <oh.seo.jun96@gmail.com>
2022-03-04 23:17:25 +0900

    03.04

---
+++ Mincoding/오서준/4.0~.py
@@ -0,0 +1,19 @@
+
+# #4-1. 입력값을 배열에 넣고 그대로 출력하기
+
+# ar=list(map(int,input().split()))
+# for i in ar:
+#     print(i,end=' ')
+    
+# # 4-2. 인덱스의 값 출력
+# ar=[4,1,3,6,9]
+# i=int(input())
+# print(f'{i}번index의값은{ar[i]}입니다')
+
+# #4-4. 플러스 알파
+
+# i=int(input())
+# ar=[]
+# for _ in range(5):
+#     ar.append(i+5)
+# print(*ar)
