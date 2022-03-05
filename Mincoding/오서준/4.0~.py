16aafd179e376af55fbdcee88230ccd4d24c26d2
오서준 <oh.seo.jun96@gmail.com>
2022-03-05 16:53:07 +0900

    03.05

--- Mincoding/오서준/4.0~.py
+++ Mincoding/오서준/4.0~.py
@@ -17,3 +17,21 @@
 # for _ in range(5):
 #     ar.append(i+5)
 # print(*ar)
+
+# # 4-5. a와 b의 합
+# arr=[3,4,1,6,7,5]
+# a,b=map(int,input().split())
+# print(arr[a]+arr[b])
+
+# # 4-6. 우와,유유
+# ar=[3,1,2,5]
+# index=int(input())
+# if 2<ar[index]:
+#     print('우와')
+# else:
+#     print('ㅠㅠ')
+
+# # 4-7. index값 입력받고 계산하기
+# ar=[3,9,27,81,243]
+# index=int(input())
+# print(ar[index]-100)
