999f613aea870daea884954e9cbd87513b5a6c69
오서준 <oh.seo.jun96@gmail.com>
2022-03-06 22:49:04 +0900

    03.06

--- Mincoding/오서준/4.0~.py
+++ Mincoding/오서준/4.0~.py
@@ -35,3 +35,26 @@
 # ar=[3,9,27,81,243]
 # index=int(input())
 # print(ar[index]-100)
+
+# # 4-8. 응용문제 If와 For 같이 써보기
+
+# a= int(input())
+# if 5<a:
+#     for i in range(1,11):
+#         print(i)
+# else:
+#     for i in range(5,0,-1):
+#         print(i)
+
+# # 4-9. 배열값 거꾸로 출력
+# ar=[5,2,3,4,7]
+# print(*ar[-1::-1])
+
+# # 4-10. 배열의 2번 index값 출력하기
+
+# ar=[0]*6
+# a=int(input())
+# for i in range(len(ar)):
+#     ar[i]=a
+#     a-=1
+# print(ar[2])
