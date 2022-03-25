ec715636986b654d1db08e43f67fb8af8a601b8f
kaydenna92 <kaydenna92@gmail.com>
2022-03-25 15:33:54 +0900

    Create 57.py
    arr = []
    for i in range(1, 1001):
    arr.append(i)
    arr = [str(i) for i in arr]이랑 str(list(range(1001))은 다른것임..

---
+++ Inflearn/나장엽/인프런100제/57.py
@@ -0,0 +1,19 @@
+# arr = []
+# for i in range(1,1001):
+#     arr.append(i)
+    
+# arr =  [str(i) for i in arr]
+
+# arr1 = str(list(range(1001)))
+# print(arr1)
+
+# count = arr.count('1')
+# print(count)
+
+
+import antigravity
+
+
+countN = str(list(range(1001))).count('1') 
+print(countN)
+#무슨 차이일까...
