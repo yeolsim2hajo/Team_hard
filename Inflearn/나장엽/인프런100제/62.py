66086f7ae862f4328dc9dcf01af7ebfeb321b183
kaydenna92 <kaydenna92@gmail.com>
2022-03-30 23:42:13 +0900

    Create 62.py

---
+++ Inflearn/나장엽/인프런100제/62.py
@@ -0,0 +1,9 @@
+string = 'aacdddddddddeee'
+
+a = string.count('a') # 2
+b = string.count('b') # 0
+c = string.count('c') # 1
+d = string.count('d') # 9
+e = string.count('e') # 3
+
+print(str(a)+str(b)+str(c)+str(d)+str(b)+str(d)+str(a)+str(e))
