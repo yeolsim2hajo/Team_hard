0393e6d9b20c6351fec660b2f912f0971fefb49b
won-kyoung <pubhan35@gmail.com>
2022-04-19 20:04:38 +0900

    '0419'

---
+++ Mincoding/나원경/0419_min_lv44.py
@@ -0,0 +1,31 @@
+#1
+# munjayeol = input()
+# alp = {}
+# for i in range(6):
+#     alp[chr(i+65)] = 0
+# for element in munjayeol:
+#     if alp.get(element.upper(),-1) != -1:
+#         alp[element.upper()] += 1
+# for key,value in alp.items():
+#     print(f'{key}:{value}')
+
+
+#2 빠른 이름검색
+# team = {'POP': 'yes','TOM': 'yes','MC': 'yes','JASON': 'yes','KFC': 'yes'}
+# N = int(input())
+# for i in range(N):
+#     print(team.get(input(),'no'))
+
+
+#3 같은 단어 찾아내기
+# hard = 'BOBOOBOBOBOBBM'
+# word_3 = {}
+# for i in range(len(hard)-2):
+#     idx = hard[i:i+3]
+#     if idx in word_3.keys(): # 값 존재
+#         word_3[idx] += 1
+#     else: # 값 존재
+#         word_3[idx] = 1
+# N = int(input())
+# for i in range(N):
+#     print(word_3.get(input(),0))
