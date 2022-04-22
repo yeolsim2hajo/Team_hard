24bd85cbe2797e7380934904a0632eeb172944ee
won-kyoung <pubhan35@gmail.com>
2022-04-22 17:58:43 +0900

    '0422'

---
+++ Mincoding/나원경/0422_min_lv45.py
@@ -0,0 +1,12 @@
+#3 SOS 모스부호
+# patterns = {'1011':True,'0011':True,'1111':True,'0000':True,'1100':True}
+# buho = [input() for _ in range(5)]
+# def y_n():
+#     for i in range(5):
+#         for j in range(2):
+#             if patterns.get(buho[i][j:j+4]):
+#                 print('yes')
+#                 return
+#     print('no')
+# y_n()
+
