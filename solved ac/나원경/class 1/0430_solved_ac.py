ac8fbab679d2f00eb2f7904e479d0f8974f2f2ab
won-kyoung <pubhan35@gmail.com>
2022-04-30 12:31:58 +0900

    0430

---
+++ solved ac/나원경/class 1/0430_solved_ac.py
@@ -0,0 +1,28 @@
+#2920 음계
+# play = input().split()
+# if play == sorted(play):
+#     print('ascending')
+# elif play == sorted(play, reverse=True):
+#     print('descending')
+# else:
+#     print('mixed')
+
+#3052 나머지
+# num_set = set()
+# for _ in range(10):
+#     N = int(input())
+#     num_set.add(N%42)
+# print(len(num_set))
+
+#8958 OX퀴즈
+# T = int(input())
+# for _ in range(T):
+#     result = input()
+#     cnt = total = 0
+#     for answer in result:
+#         if answer == 'X':
+#             cnt = 0
+#         else:
+#             cnt += 1
+#             total += cnt
+#     print(total)
