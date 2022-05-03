5155b84c4150884dbe27e6400f9eda964049879e
won-kyoung <pubhan35@gmail.com>
2022-05-03 20:11:25 +0900

    '0503'

---
+++ solved ac/나원경/class 1/0503_solved_ac.py
@@ -0,0 +1,22 @@
+#1152 단어의 개수
+# sentence = input().strip().split()
+# print(len(sentence))
+
+#10951 A+B - 4
+
+
+#10952 A+B - 5
+while 1:
+    A, B = map(int,input().split())
+    if A == B == 0:
+        break
+    else:
+        print(A+B)
+
+# 11720 숫자의 합
+# N = int(input())
+# numbers = input()
+# total = 0
+# for i in range(N):
+#     total += int(numbers[i])
+# print(total)
