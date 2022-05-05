e44fd725d234de6a55922f40fd30dc60522076ca
나장엽 <kaydenna92@gmail.com>
2022-05-05 23:51:45 +0900

    Update 10809.py

--- solved ac/나장엽/class1/10809.py
+++ solved ac/나장엽/class1/10809.py
@@ -1,1 +1,14 @@
+S = list(input())
+check = [-1]*26
+alpha = []
+for i in range(97, 123):
+    alpha.append(chr(i))
 
+for i in range(len(S)):
+    for k in range(len(alpha)):
+        if check[k] != -1:
+                continue
+        if S[i] == alpha[k]:
+            check[k] = i
+
+print(*check, sep = ' ')
