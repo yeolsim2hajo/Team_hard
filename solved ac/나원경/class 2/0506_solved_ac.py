41d0264d9a1eb26c4bd9113c1ca686f4e5c7dcb3
won-kyoung <pubhan35@gmail.com>
2022-05-06 18:17:22 +0900

    0506

---
+++ solved ac/나원경/class 2/0506_solved_ac.py
@@ -0,0 +1,9 @@
+#1259 팰린드롬수
+# while 1:
+#     number = input()
+#     if number == '0':
+#         break
+#     elif number == number[::-1]:
+#         print('yes')
+#     else:
+#         print('no')
