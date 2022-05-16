088a1feb53886114a148a95d3dc49b30157250a1
won-kyoung <pubhan35@gmail.com>
2022-05-16 22:54:16 +0900

    0516

---
+++ solved ac/나원경/class 2/0516_solved_ac.py
@@ -0,0 +1,12 @@
+#10173 제로
+# K = int(input())
+# total = 0
+# numbers = []
+# for _ in range(K):
+#     now = int(input())
+#     if now:
+#         total += now
+#         numbers.append(now)
+#     else:
+#         total -= numbers.pop()
+# print(total)
