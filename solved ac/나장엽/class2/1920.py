cc8853410b9c013da60969d69bf35ef355e86f38
나장엽 <kaydenna92@gmail.com>
2022-05-16 18:05:56 +0900

    create 1929

--- solved ac/나장엽/class2/1920.py
+++ solved ac/나장엽/class2/1920.py
@@ -1,19 +1,20 @@
 
 import sys
 input = sys.stdin.readline
-
 N = int(input())
-numbers1 = list(map(int, input().split()))
+numbers1 = set(map(int, input().split()))
 M = int(input())
 numbers2 = list(map(int, input().split()))
 
-numbers1_idx = 0
-numbers2_idx = 0
-for i in range(M):
-    for k in range(N):
-        if numbers2[i] == numbers1[k]:
-            print(1)
-    else:
-        print(0)
+# 시간초과
+# for i in range(M):
+#     for k in range(N):
+#         if numbers2[i] == numbers1[k]:
+#             print(1)
+#             break
+#     else:
+#         print(0)
            
             
+for number2 in numbers2:
+    print(1) if number2 in numbers1 else print(0)
