dc2864cd44749ca75799d1e56c864354f45eece0
나장엽 <kaydenna92@gmail.com>
2022-05-30 20:37:04 +0900

    Create 2869.py

---
+++ solved ac/나장엽/class2/2869.py
@@ -0,0 +1,16 @@
+import math 
+A, B, V = map(int, input().split())
+
+# 시간초과
+# day = 0
+# climing = 0
+# while True:
+#     day += 1
+#     climing += A
+#     if climing == V:
+#         print(day)
+#         break
+#     climing -= B
+
+day = (V- B) / (A - B)
+print(math.ceil(day)) # 올림 처리 5.1일은 6일 걸린것.
