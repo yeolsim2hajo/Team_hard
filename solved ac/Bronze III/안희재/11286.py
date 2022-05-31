6ed0604b1c9a26d45048c9db340737cd4fc5a241
mario <marioahn@naver.com>
2022-05-31 22:31:36 +0900

    0531hj

---
+++ solved ac/Bronze III/안희재/11286.py
@@ -0,0 +1,11 @@
+N, K = map(int, input().split()) 
+coin_lst = list()
+for i in range(N):
+    coin_lst.append(int(input()))
+
+count = 0
+for i in reversed(range(N)):
+    count += K//coin_lst[i]
+    K = K%coin_lst[i] 
+
+print(count)
