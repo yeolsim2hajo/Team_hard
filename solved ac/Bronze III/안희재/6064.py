b3263dbba109d7c6f211e47e669c0a8993097beb
mario <marioahn@naver.com>
2022-06-08 21:02:16 +0900

    0608hj

---
+++ solved ac/Bronze III/안희재/6064.py
@@ -0,0 +1,11 @@
+def num(m, n, x, y):
+    while x <= m * n:
+        if (x - y) % n == 0:
+            return x
+        x += m
+    return -1
+
+t = int(input())
+for i in range(t):
+    m, n, x, y = map(int, input().split())
+    print(num(m, n, x, y))
