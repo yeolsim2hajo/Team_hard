2baa2605f1732edd8da159049e6e71cb097a2c79
han <qkrgks378@gmail.com>
2022-07-03 20:54:01 +0900

    Create tony9402_0703_2138.py

---
+++ solved ac/박한/tony9402/tony9402_0703_2138.py
@@ -0,0 +1,9 @@
+# 백준 2138 전구와 스위치
+
+import sys
+input = sys.stdin.readline
+n = int(input())
+lst1 = list(map(int, input().rstrip("\n")))
+lst2 = list(map(int, input().rstrip("\n")))
+
+# 몇 번 손대다가 포기..
