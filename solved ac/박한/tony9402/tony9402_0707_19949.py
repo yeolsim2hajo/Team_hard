24ba36f7df617e54104e2402a5abfbc0831a9157
han <qkrgks378@gmail.com>
2022-07-08 00:27:49 +0900

    Update tony9402_0707_19949.py

--- solved ac/박한/tony9402/tony9402_0707_19949.py
+++ solved ac/박한/tony9402/tony9402_0707_19949.py
@@ -1,7 +1,25 @@
 # 백준 19949 영재의 시험
 
-import sys
-input = sys.stdin.readline
 lst = list(map(int, input().split()))
+lst1 = []
+ans = 0
 
-# 못 풀겠다..
+def dfs(depth):
+    global ans
+    if depth == 10:
+        rst = 0
+        for i in range(10):
+            if lst1[i] == lst[i]:
+                rst += 1
+        if rst >= 5:
+            ans += 1
+        return ;
+    for n in range(1, 6):
+        if depth > 1 and lst1[depth-2] == lst1[depth-1] == n:
+            continue
+        lst1.append(n)
+        dfs(depth+1)
+        lst1.pop()
+
+dfs(0)
+print(ans)
