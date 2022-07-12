d39c86401c552bad37bf9c70f8e73a15cf45e559
won-kyoung <pubhan35@gmail.com>
2022-07-12 21:52:37 +0900

    0712 wk

--- solved ac/나원경/recommended/0712_solved_ac.py
+++ solved ac/나원경/recommended/0712_solved_ac.py
@@ -1,26 +1,67 @@
-#1972 놀라운 문자열
-def main():
-    import sys
-    new_input = sys.stdin.readline
-    def is_surprising():
-        for i in range(1,n-1):
-            arr = set()
-            for j in range(n-i):
-                new = data[j] + data[j+i]
-                if new in arr:
-                    return 0
-                arr.add(new)
-        return 1
+#1972 놀라운 문자열 - new_input 안 쓰면 시간 더 늘어남
+# def main():
+#     import sys
+#     new_input = sys.stdin.readline
+#     def is_surprising():
+#         for i in range(1,n-1):
+#             arr = set()
+#             for j in range(n-i):
+#                 new = data[j] + data[j+i]
+#                 if new in arr:
+#                     return 0
+#                 arr.add(new)
+#         return 1
+#
+#     while True:
+#         data = new_input().rstrip()
+#         n = len(data)
+#         if data == '*':
+#             return
+#         if is_surprising():
+#             print(f'{data} is surprising.')
+#         else:
+#             print(f'{data} is NOT surprising.')
+# main()
 
-    while True:
-        data = new_input().rstrip()
-        n = len(data)
-        if data == '*':
-            return
-        if is_surprising():
-            print(f'{data} is surprising.')
-        else:
-            print(f'{data} is NOT surprising.')
-main()
+# print 위치 바꿈 - 약간 더 걸림
+# def main():
+#     def is_surprising():
+#         for i in range(1,n-1):
+#             arr = set()
+#             for j in range(n-i):
+#                 new = data[j] + data[j+i]
+#                 if new in arr:
+#                     print(f'{data} is NOT surprising.')
+#                     return
+#                 arr.add(new)
+#         print(f'{data} is surprising.')
+#
+#     while True:
+#         data = input()
+#         n = len(data)
+#         if data == '*':
+#             return
+#         is_surprising()
+# main()
 
 
+# 시간 약간 더 걸림
+# def main():
+#     def is_surprising():
+#         for i in range(1,n-1):
+#             arr = set()
+#             for j in range(n-i):
+#                 new = data[j] + data[j+i]
+#                 if new in arr:
+#                     return 0
+#                 arr.add(new)
+#         return 1
+#     answer = ['is NOT surprising.', 'is surprising.']
+#     while True:
+#         data = input()
+#         n = len(data)
+#         if data == '*':
+#             return
+#         print(data,answer[is_surprising()])
+#
+# main()
