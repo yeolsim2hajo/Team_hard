8c61f70c0d67dc58668396b633ba0c6b5ef532ec
나장엽 <kaydenna92@gmail.com>
2022-07-22 00:06:00 +0900

    Create 1193.py

---
+++ solved ac/나장엽/구현/1193.py
@@ -0,0 +1,17 @@
+# 분수 찾기
+input_num = int(input())
+
+line = 0  # 사선 라인
+max_num = 0  # 입력된 숫자(input_num 변수)의 라인에서 가장 큰 숫자
+while input_num > max_num:
+    line += 1  
+    max_num += line  
+
+gap = max_num - input_num 
+if line % 2 == 0:  # 사선 라인이 짝수번째 일 때
+    top = line - gap  #분자
+    under = gap + 1  #분모
+else :  # 사선 라인이 홀수번째 일 때
+    top = gap + 1  #분자
+    under = line - gap  #분모
+print(f'{top}/{under}')
