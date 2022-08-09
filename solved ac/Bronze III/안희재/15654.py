8275972a735c4cdbe84974cce354ad7a2291cf87
mario <marioahn@naver.com>
2022-08-09 19:13:39 +0900

    0809hj

--- solved ac/Bronze III/안희재/15654.py
+++ solved ac/Bronze III/안희재/15654.py
@@ -5,14 +5,14 @@ N, M = map(int,input().split())
 arr = list(map(int,input().split()))
 
 arr.sort()
-# dfs인가?
+# dfs
 check = [0] * N
 result = []
 
 def dfs(level):
     if level == M:
-        print(' '.join(map(str, result))) # 여기 구현에서 애먹었음;; 너무 오랜만이었..
-        return # 여기도;;
+        print(' '.join(map(str, result))) 
+        return
     
     for k in range(N):
         if check[k] == 0:
