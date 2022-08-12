18feaada5e6c8a21211d125419cedba8aeed8dbb
mario <marioahn@naver.com>
2022-08-12 18:11:58 +0900

    -0812hj

---
+++ solved ac/Bronze III/안희재/1929.py
@@ -0,0 +1,10 @@
+x, y = map(int, input().split())
+
+for i in range(x, y+1):
+    if i == 1: #1은 소수가 아뉘지!
+        continue
+    for j in range(2, int(i** 0.5)+1 ):
+        if i%j==0:
+            break
+    else:
+        print(i)
