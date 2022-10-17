60b4d6462ba2b527e3c016b0c92790d03697260e
won-kyoung <pubhan35@gmail.com>
2022-10-17 21:15:23 +0900

    [BOJ] 14592_2017_아주대학교_프로그래밍_경시대회(Small)

--- wonkyoung/solved ac/heap/14592_2017_아주대학교_프로그래밍_경시대회(Small).py
+++ wonkyoung/solved ac/heap/14592_2017_아주대학교_프로그래밍_경시대회(Small).py
@@ -32,3 +32,39 @@ def find_winner():
     return heap[0][-1]
 print(find_winner() + 1)
 
+# heapify
+def find_winner():
+    from heapq import heapify, heappop
+    N = int(input())
+    heap = []
+    for i in range(N):
+        score, submit_cnt, upload_time = map(int, input().split())
+        heap.append((-score, submit_cnt, upload_time, i))
+    heapify(heap)
+    return heappop(heap)[-1]
+print(find_winner() + 1)
+
+# 한꺼번에 받기
+def find_winner():
+    from heapq import heappush, heappop
+    N = int(input())
+    heap = []
+    for i in range(N):
+        info = list(map(int, input().split()))
+        info[0] = -info[0]
+        heappush(heap, (*info, i))
+    return heappop(heap)[-1]
+print(find_winner() + 1)
+
+# return 값 조절
+def find_winner():
+    from heapq import heappush, heappop
+    N = int(input())
+    heap = []
+    for i in range(N):
+        info = list(map(int, input().split()))
+        info[0] = -info[0]
+        heappush(heap, (*info, i))
+    return heappop(heap)
+print(find_winner()[-1] + 1)
+
