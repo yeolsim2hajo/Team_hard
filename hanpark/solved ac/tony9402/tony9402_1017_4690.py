e862457b06fcf1399456db30d275216837fdfbcf
han <qkrgks378@gmail.com>
2022-10-17 22:43:03 +0900

    Create tony9402_1017_4690.py

---
+++ hanpark/solved ac/tony9402/tony9402_1017_4690.py
@@ -0,0 +1,9 @@
+# 백준 4690 완전 세제곱
+
+for a in range(2,101):
+    for b in range(2,101):
+        for c in range(b+1,101):
+            for d in range(c+1,101):
+                if a*a*a==(b*b*b+c*c*c+d*d*d):
+                    print("Cube = {}, Triple = ({},{},{})".format(a,b,c,d))
+                if a*a*a<(b*b*b+c*c*c+d*d*d):break
