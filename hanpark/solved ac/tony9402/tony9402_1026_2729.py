fb61bfd5ae62a9a302552f7430678afd0bb71908
han <qkrgks378@gmail.com>
2022-10-26 23:59:46 +0900

    Create tony9402_1026_2729.py

---
+++ hanpark/solved ac/tony9402/tony9402_1026_2729.py
@@ -0,0 +1,10 @@
+# 백준 2729 이진수 덧셈
+
+import sys
+input = sys.stdin.readline
+n = int(input())
+
+for i in range(n):
+    a, b = map(str,input().split())
+    c = int(a,2) + int(b,2)
+    print(bin(c)[2:])
