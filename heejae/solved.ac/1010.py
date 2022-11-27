e663dfb245cc06288fa27ca0cbf17e9088256592
mario <marioahn@naver.com>
2022-11-28 05:12:46 +0900

    1128hj

---
+++ heejae/solved.ac/1010.py
@@ -0,0 +1,8 @@
+import math
+
+T = int(input())
+
+for _ in range(T):
+    n, m = map(int, input().split())
+    bridge = math.factorial(m) // (math.factorial(n) * math.factorial(m - n))
+    print(bridge)
