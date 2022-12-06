a70d8b1de6b7d61c18d7b4067001295cc07f495b
han <qkrgks378@gmail.com>
2022-12-07 01:04:38 +0900

    Create tony9402_1206_21918.py

---
+++ hanpark/solved ac/tony9402/tony9402_1206_21918.py
@@ -0,0 +1,26 @@
+# 백준 1206 21918 전구
+
+import sys
+input = sys.stdin.readline
+n, m = map(int,input().split())
+bulb = [0] + list(map(int,input().split()))
+for _ in range(m):
+	a, b, c = map(int, input().split())
+	if a == 1:
+		bulb[b] = c
+	elif a == 2:
+		for i in range(b, c+1):
+			if bulb[i] == 0:
+				bulb[i] = 1
+			elif bulb[i] == 1:
+				bulb[i] = 0
+	elif a == 3:
+		for i in range(b, c+1):
+			if bulb[i] == 1:
+				bulb[i] = 0
+	elif a == 4:
+		for i in range(b, c+1):
+			if bulb[i] == 0:
+				bulb[i] = 1
+for i in range(1, n+1):
+	print(bulb[i], end=' ')
