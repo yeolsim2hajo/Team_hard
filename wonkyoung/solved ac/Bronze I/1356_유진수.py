4ffd903dced754976b939b2feff5063a18b98bbd
won-kyoung <pubhan35@gmail.com>
2023-01-01 23:17:10 +0900

    [BOJ] 1356_유진수

---
+++ wonkyoung/solved ac/Bronze I/1356_유진수.py
@@ -0,0 +1,34 @@
+#230101
+# N = input()
+# def find_multiple(a, b):
+#     final_a = final_b = 1
+#     for j in a:
+#         final_a *= int(j)
+#     for j in b:
+#         final_b *= int(j)
+#     return final_a == final_b
+#
+# for i in range(1, len(N)):
+#     if find_multiple(N[:i], N[i:]):
+#         print('YES')
+#         break
+# else:
+#     print('NO')
+
+
+# 함수
+# def main():
+#     N = input()
+#     def find_multiple(a, b):
+#         final_a = final_b = 1
+#         for j in a:
+#             final_a *= int(j)
+#         for j in b:
+#             final_b *= int(j)
+#         return final_a == final_b
+#
+#     for i in range(1, len(N)):
+#         if find_multiple(N[:i], N[i:]):
+#             return 'YES'
+#     return 'NO'
+# print(main())
