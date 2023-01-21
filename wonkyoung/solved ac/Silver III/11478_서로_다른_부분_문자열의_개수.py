91648025c804fe9d160f55e0fdaa741eecd011a8
won-kyoung <pubhan35@gmail.com>
2023-01-21 15:30:29 +0900

    [BOJ] 11478_서로_다른_부분_문자열의_개수

---
+++ wonkyoung/solved ac/Silver III/11478_서로_다른_부분_문자열의_개수.py
@@ -0,0 +1,20 @@
+#230121
+# set 사용 - 길이, start
+# str_set = set()
+# S = input()
+# N = len(S)
+# for cnt in range(1, N+1):
+#     for start in range(N-cnt+1):
+#         str_set.add(S[start:start+cnt])
+# # print(str_set)
+# print(len(str_set))
+
+# start, end
+# str_set = set()
+# S = input()
+# N = len(S)
+# for start in range(N):
+#     for end in range(start+1, N+1):
+#         str_set.add(S[start:end])
+# print(len(str_set))
+
