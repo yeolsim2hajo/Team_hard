b94962c86e8eb2619be20145c80cc9db27a519b6
won-kyoung <pubhan35@gmail.com>
2023-02-03 18:17:21 +0900

    [BOJ] 4096_팰린드로미터

--- wonkyoung/solved ac/Silver V/4096_팰린드로미터.py
+++ wonkyoung/solved ac/Silver V/4096_팰린드로미터.py
@@ -26,6 +26,33 @@ while True:
                 print(cnt)
                 break
 
+# 함수형
+def make_palindrome():
+    if N == N[::-1]:
+        return 0
+    to_next = {str(i): str(i+1) for i in range(9)}
+    num = list(map(str, N))
+    length, cnt = len(num), 0
+    while True:
+        cnt += 1
+        for i in range(length-1, -1, -1):
+            element = num[i]
+            if to_next.get(element):
+                num[i] = to_next[element]
+                break
+            else:
+                num[i] = '0'
+        if num == num[::-1]:
+            return cnt
+
+from sys import stdin
+new_input = stdin.readline
+while True:
+    N = new_input().rstrip()
+    if N == '0':
+        break
+    print(make_palindrome())
+
 
 
 
