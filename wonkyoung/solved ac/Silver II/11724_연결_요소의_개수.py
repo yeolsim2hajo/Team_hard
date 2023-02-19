537590e226ad32a7d6cbb115d4dc08748e46a7c3
pubhan35@gmail.com <pubhan35@gmail.com>
2023-02-19 17:27:10 +0900

    [BOJ] 11724_연결_요소의_개수

--- wonkyoung/solved ac/Silver II/11724_연결_요소의_개수.py
+++ wonkyoung/solved ac/Silver II/11724_연결_요소의_개수.py
@@ -1,38 +1,38 @@
 #230219
-def find_length():
-    from sys import stdin
-    new_input = stdin.readline
-    N, M = map(int, new_input().split())
-    root_of = list(range(N+1))
-    def find(node):
-        if node == root_of[node]:
-            return node
-        root = find(root_of[node])
-        root_of[node] = root
-        return root
-    def union(u, v):
-        root_u = find(u)
-        root_v = find(v)
-        if root_u == root_v:
-            return
-        elif root_u < root_v:
-            root_of[root_v] = root_u
-        else:
-            root_of[root_u] = root_v
-
-    for _ in range(M):
-        u, v = map(int, new_input().split())
-        union(u, v)
-
-    root_set = set()
-    for node in range(1, N+1):
-        find(node)
-        root = root_of[node]
-        root_set.add(root)
-
-    return len(root_set)
-
-print(find_length())
+# def find_length():
+#     from sys import stdin
+#     new_input = stdin.readline
+#     N, M = map(int, new_input().split())
+#     root_of = list(range(N+1))
+#     def find(node):
+#         if node == root_of[node]:
+#             return node
+#         root = find(root_of[node])
+#         root_of[node] = root
+#         return root
+#     def union(u, v):
+#         root_u = find(u)
+#         root_v = find(v)
+#         if root_u == root_v:
+#             return
+#         elif root_u < root_v:
+#             root_of[root_v] = root_u
+#         else:
+#             root_of[root_u] = root_v
+#
+#     for _ in range(M):
+#         u, v = map(int, new_input().split())
+#         union(u, v)
+#
+#     root_set = set()
+#     for node in range(1, N+1):
+#         find(node)
+#         root = root_of[node]
+#         root_set.add(root)
+#
+#     return len(root_set)
+#
+# print(find_length())
 '''
 6 7
 1 2
@@ -53,3 +53,77 @@ print(find_length())
 7 8
 11 10
 '''
+
+
+# rank 이용
+# def find_length():
+#     from sys import stdin
+#     new_input = stdin.readline
+#     N, M = map(int, new_input().split())
+#     root_of = list(range(N+1))
+#     rank = [0] * (N+1)
+#     def find(node):
+#         if node == root_of[node]:
+#             return node
+#         root = find(root_of[node])
+#         root_of[node] = root
+#         return root
+#     def link (u, v):
+#         if rank[u] > rank[v]:
+#             root_of[v] = u
+#         else:
+#             root_of[u] = v
+#             if rank[u] == rank[v]:
+#                 rank[v] += 1
+#     def union(u, v):
+#         root_u = find(u)
+#         root_v = find(v)
+#         if root_u == root_v:
+#             return
+#         link(root_u, root_v)
+#
+#     for _ in range(M):
+#         u, v = map(int, new_input().split())
+#         union(u, v)
+#
+#     root_set = set()
+#     for node in range(1, N+1):
+#         find(node)
+#         root = root_of[node]
+#         root_set.add(root)
+#     return len(root_set)
+#
+# print(find_length())
+
+# 기준 X
+def find_length():
+    from sys import stdin
+    new_input = stdin.readline
+    N, M = map(int, new_input().split())
+    root_of = list(range(N+1))
+    def find(node):
+        if node == root_of[node]:
+            return node
+        root = find(root_of[node])
+        root_of[node] = root
+        return root
+    def union(u, v):
+        root_u = find(u)
+        root_v = find(v)
+        if root_u == root_v:
+            return
+        root_of[root_v] = root_u
+
+    for _ in range(M):
+        u, v = map(int, new_input().split())
+        union(u, v)
+
+    root_set = set()
+    for node in range(1, N+1):
+        find(node)
+        root = root_of[node]
+        root_set.add(root)
+
+    return len(root_set)
+
+print(find_length())
