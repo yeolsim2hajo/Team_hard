c1c333fae2e3e41f5ac33b77d7335033c04c0d95
pubhan35@gmail.com <pubhan35@gmail.com>
2023-03-08 23:33:44 +0900

    [BOJ] 1037_약수

--- wonkyoung/solved ac/Bronze I/1037_약수.py
+++ wonkyoung/solved ac/Bronze I/1037_약수.py
@@ -11,3 +11,31 @@
 #     answer = (numbers[0])**2
 # print(answer)
 
+#230308
+# N = int(input())
+# div_num = list(map(int, input().split()))
+# print(max(div_num) * min(div_num))
+
+
+#
+# N = int(input())
+# max_num, min_num = 0, 1e6
+# for num in map(int, input().split()):
+#     if max_num < num:
+#         max_num = num
+#     if min_num > num:
+#         min_num = num
+# print(max_num * min_num)
+
+
+#
+def find_num():
+    N = int(input())
+    max_num, min_num = 0, 1e6
+    for num in map(int, input().split()):
+        if max_num < num:
+            max_num = num
+        if min_num > num:
+            min_num = num
+    return max_num * min_num
+print(find_num())
